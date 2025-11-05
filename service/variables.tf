fb50527d4cc29d18fa874ee1e3887e3a51cef2f1
Sai Charan <saicharan.sudo@gmail.com>
2025-11-05 22:00:53 +0530

    Remove redundant AWS provider block from variables.tf to fix initialization error

--- service/variables.tf
+++ service/variables.tf
@@ -1,12 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
+variable "region" {
+  type        = string
+  default     = "us-east-1"
+  description = "AWS region"
 }
